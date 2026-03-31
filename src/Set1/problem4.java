62b79b2a0ecdd8d9a29073d003feab24dcd930ea
Naziat Khan <khannaziat54@gmail.com>
2026-03-31 23:17:41 +0600

    Set 1 : Problem 5

--- src/Set1/problem4.java
+++ src/Set1/problem4.java
@@ -14,9 +14,9 @@ public class problem4 {
         int num2 = myObject.nextInt();
         int num3 = myObject.nextInt();
 
-        if (num1 > num2 && num1 > num3) {
+        if (num1 >= num2 && num1 >= num3) {
             System.out.println(num1 + " is the largest");
-        } else if (num2 > num1 && num2 > num3) {
+        } else if (num2 >= num1 && num2 >= num3) {
             System.out.println(num2 + " is the largest");
         } else {
             System.out.println(num3 + " is the largest");
---
+++ src/Set1/problem5.java
@@ -0,0 +1,35 @@
+
+package Set1;
+import java.util.Scanner;
+public class problem5 {
+    public static void main(String[] args) {
+
+        Scanner myObject= new Scanner(System.in);
+        
+        System.out.print("Enter two numbers = ");
+        int num1= myObject.nextInt();
+        int num2= myObject.nextInt();
+        
+        System.out.print("Select Operator = ");
+        char operator =myObject.next().charAt(0);
+        
+        if( operator== '+')
+        {
+            System.out.println(num1+" + "+num2+" = "+(num1+num2));
+        }
+        if( operator== '-')
+        {
+            System.out.println(num1+" - "+num2+" = "+(num1-num2));
+        }
+        if( operator== '*')
+        {
+            System.out.println(num1+" * "+num2+" = "+(num1*num2));
+        }
+        if( operator== '/')
+        {
+            System.out.println(num1+" / "+num2+" = "+(num1/num2));
+        }
+
+    }
+    
+}
