71b0aad37948832b850d9d06879e0422ba2f3189
Naziat Khan <khannaziat54@gmail.com>
2026-03-31 21:16:20 +0600

    Set 1 : Problem 1

--- src/Set1/problem1.java
+++ src/Set1/problem1.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Set1;
 
-/**
- *
- * @author Naziat Khan
- */
 public class problem1 {
-    
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+    }
 }
