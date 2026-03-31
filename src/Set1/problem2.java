3969b25beaa06a43e3451a4faec4a4a3ebf4cde3
Naziat Khan <khannaziat54@gmail.com>
2026-03-31 21:33:14 +0600

    Set 1 : Problem 2

---
+++ src/Set1/problem2.java
@@ -0,0 +1,16 @@
+
+package Set1;
+import java.util.Scanner;
+
+public class problem2 {
+    public static void main(String[] args) {
+        Scanner myObject= new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        
+        String name= myObject.nextLine();
+
+        System.out.println("Hello, "+name+"!");
+
+    }
+    
+}
