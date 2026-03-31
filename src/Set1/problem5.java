65287852ab0402e066895c6284403f603d3461d9
Naziat Khan <khannaziat54@gmail.com>
2026-03-31 23:28:07 +0600

    Set 1 : Problem 6

--- src/Set1/problem5.java
+++ src/Set1/problem5.java
@@ -1,35 +1,33 @@
-
 package Set1;
+
 import java.util.Scanner;
+
 public class problem5 {
+
     public static void main(String[] args) {
 
-        Scanner myObject= new Scanner(System.in);
-        
+        Scanner myObject = new Scanner(System.in);
+
         System.out.print("Enter two numbers = ");
-        int num1= myObject.nextInt();
-        int num2= myObject.nextInt();
-        
+        int num1 = myObject.nextInt();
+        int num2 = myObject.nextInt();
+
         System.out.print("Select Operator = ");
-        char operator =myObject.next().charAt(0);
-        
-        if( operator== '+')
-        {
-            System.out.println(num1+" + "+num2+" = "+(num1+num2));
+        char operator = myObject.next().charAt(0);
+
+        if (operator == '+') {
+            System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
         }
-        if( operator== '-')
-        {
-            System.out.println(num1+" - "+num2+" = "+(num1-num2));
+        if (operator == '-') {
+            System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
         }
-        if( operator== '*')
-        {
-            System.out.println(num1+" * "+num2+" = "+(num1*num2));
+        if (operator == '*') {
+            System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
         }
-        if( operator== '/')
-        {
-            System.out.println(num1+" / "+num2+" = "+(num1/num2));
+        if (operator == '/') {
+            System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
         }
 
     }
-    
+
 }
---
+++ src/Set1/problem6.java
@@ -0,0 +1,22 @@
+
+package Set1;
+
+import java.util.Scanner;
+
+public class problem6 {
+    public static void main(String[] args) {
+
+        Scanner myObject = new Scanner(System.in);
+
+        System.out.print("Enter n = ");
+
+        int n = myObject.nextInt();
+        int i;
+        for(i=1;i<=n;i++)
+        {
+            System.out.println(i);
+        }
+
+    }
+    
+}
